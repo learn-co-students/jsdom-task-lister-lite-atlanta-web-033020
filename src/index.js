d77a672a5eab4b8a26ab12d0c229dfb75b956daa
github username <github email address>
2020-05-13 14:21:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,71 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', function () {
   init();
 });
 
-let listItems = [];
-
 function init() {
-  submitForm();
+  formControl();
 }
 
-function submitForm() {
-  form = document.querySelector('#create-task-form');
+// Initialize variable to store list items
+const listItems = [];
+
+function formControl() {
+  // Define form and new task HTML elements
+  const form = document.querySelector('#create-task-form');
+  const newTask = document.querySelector('#new-task-description');
+
   form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    listItems.push(document.querySelector('#new-task-description').value);
-    renderListItems();
+    // Add item to array
+    addItem(newTask.value);
 
+    // Render array into li elements
+    renderList();
+
+    // Reset form
     e.target.reset();
   });
 }
 
-function renderListItems() {
-  const ul = document.querySelector('#tasks');
-  ul.innerHTML = '';
-  listItems.forEach(function (item) {
-    const li = document.createElement('li');
-    const button = document.createElement('button');
-    button.addEventListener('click', function (e) {
-      deleteListItem(item);
-      e.target.parentElement.remove();
-    });
-    button.innerHTML = 'x';
-    li.innerHTML = `${item}  `;
-    li.appendChild(button);
-    ul.appendChild(li);
-  })
+function addItem(item) {
+  // Add item to array
+  listItems.push(item);
 }
 
-function deleteListItem(item) {
+function deleteItem(item) {
+  // Loop through listItems and remove element from array
   for (let i = 0; i < listItems.length; i++) {
     if (listItems[i] === item) {
       listItems.splice(i, 1);
+      break;
     }
   }
+}
+
+function renderList() {
+  // Find tasks ul and clear contents
+  const ul = document.querySelector('#tasks');
+  ul.innerHTML = '';
+
+  // Create list items
+  listItems.forEach(function (item) {
+    // Create li and write task item
+    const li = document.createElement('li');
+    li.innerHTML = `${item}  `
+
+    // Create delete button with interaction
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'x';
+    deleteButton.addEventListener('click', (e) => {
+      e.target.parentElement.remove();
+      deleteItem(item);
+    });
+
+    // Append delete button to li
+    li.appendChild(deleteButton);
+
+    // Append li to ul
+    ul.appendChild(li);
+  });
 }
